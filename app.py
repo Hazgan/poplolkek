23b60e5ac11b4963b9a685b3ec00ee83daa31216
Hazgan <57675550+Hazgan@users.noreply.github.com>
2020-12-03 20:07:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,40 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/haba')
+def menu():
+    out = """<pre>
+    <input type="search" id="menu">
+    <datalist id="character">
+    <option value="1"></option>
+    <option value="2"></option>
+    <option value="3"></option>
+   </datalist>
+   </pre>"""
+    return out
+
+
+@app.route('/haba/')
 def hello_world():
+    s = ["Hello, Haba!",
+         "Hello, Arsen!",
+         "Hello, Karim!"]
+
+    out = "<pre>{}</pre>".format("\n".join(s))
+    return out
+
+
+@app.route('/task1/random/')
+def random():
+    s = ["Hello, Haba!",
+         "Hello, Arsen!",
+         "Hello, Karim!"]
+
+    out = "<pre>{}</pre>".format("\n".join(s))
+    return out
+
+
+@app.route('/task1/i_will_not/')
+def i_will_not():
     s = ["Hello, Haba!",
          "Hello, Arsen!",
          "Hello, Karim!"]
