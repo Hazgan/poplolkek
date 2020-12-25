4b8aad15db8a41d23ac99fba0180484d7cbad273
Hazgan <57675550+Hazgan@users.noreply.github.com>
2020-12-25 13:12:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,59 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from random import randint
 import requests
 import inflect
 import json
+import random as rnd
+import string
+from collections import OrderedDict
+from operator import itemgetter
 
 app = Flask(__name__)
+
+s = rnd.choice(string.ascii_letters) + rnd.choice(string.ascii_letters)
+
+
+@app.route('/task3/cf/profile/<handle>/')
+def cf_si(handle):
+    return redirect(url_for('cf_single', handle=handle, page_number=1))
+
+
+@app.route('/task3/cf/profile/<handle>/page/<int:page_number>/')
+def cf_single(handle, page_number):
+    url = f'http://codeforces.com/api/user.status?handle={handle}&from=1&count=100'
+    text = requests.get(url).text
+    ssilka = json.loads(text)
+    popitki = ssilka["result"]
+
+    max_page_number = (len(popitki) + 24) // 25
+    return render_template("cf_single_page.html", popitki=popitki, handle=handle, max_page_number=max_page_number,
+                           page_number=page_number)
+
+
+@app.route('/task3/cf/top/')
+def top():
+    handles = sorted(request.args.get("handles").split("|"))
+    orderby = request.args.get("orderby", "")
+    handict = {}
+    url = "https://codeforces.com/api/user.info?handles="
+    for nick in handles:
+        url = url + nick + ";"
+    ssilka = json.loads(requests.get(url).text)
+    if ssilka["status"] == "FAILED":
+        return "User not found"
+    else:
+        for nicki in ssilka["result"]:
+            handle = nicki["handle"]
+            rating = nicki["rating"]
+            handict[handle] = int(rating)
+        if orderby == "rating":
+            handict = OrderedDict(sorted(handict.items(), key=itemgetter(1), reverse=True))
+    return render_template("cf_top.html", dict=handict)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("error404.html")
 
 
 @app.route('/task2/num2words/<num>/')
